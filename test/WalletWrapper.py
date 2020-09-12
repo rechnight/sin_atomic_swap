91896f0f8c873cd0382c83159fd88684cd6ff145
rechnight <57619271+rechnight@users.noreply.github.com>
2020-09-12 20:28:24 -0300

    Update WalletWrapper.py

--- test/WalletWrapper.py
+++ test/WalletWrapper.py
@@ -5,32 +5,23 @@ sdkWrapper = SdkWrapper.GetInstance()
 
 class WalletManager():
     def __init__(self):
-        self.alice = Alice()
-        self.aliceAddr = AliceAddress()
-        self.bobAddr = BobAddress()
-        self.bob = Bob()
-        self.eveAddr = EveAddress()
-        self.eve = Eve()
+        self.singames = SinGames()
+        self.singamesAddr = SinGamesAddress()
+        self.furnaceAddr = FurnaceAddress()
+        self.furnace = Furnace()
         
 
 
-def Alice():
-    return sdkWrapper.GetSdk().wallet_manager.get_account(AliceAddress(), 'password')
+def SinGames():
+    return sdkWrapper.GetSdk().wallet_manager.get_account(SinGamesAddress(), 'password')
 
-def Bob():
-    return sdkWrapper.GetSdk().wallet_manager.get_account(BobAddress(), 'password')
-
-def Eve():
-    return sdkWrapper.GetSdk().wallet_manager.get_account(EveAddress(), 'test')
+def Furnace():
+    return sdkWrapper.GetSdk().wallet_manager.get_account(FurnaceAddress(), 'password')
 
 
 
-def AliceAddress():
-    # return 'AecaeSEBkt5GcBCxwz1F41TvdjX3dnKBkJ'
-    return 'AUr5QUfeBADq6BMY6Tp5yuMsUNGpsD7nLZ'
+def SinGamesAddress():
+    return 'AM82aCCE1NyE7cmpwpRzbcuvr3ndXHCq8Q'
 
-def BobAddress():
-    return 'AQvZMDecMoCi2y4V6QKdJBtHW1eV7Vbaof'
-
-def EveAddress():
-    return "AVW5gqPewjmZ5W1osaU2CmMB2iamBHY2CA"
+def FurnaceAddress():
+    return 'ASz7Rr7YWMJrNkCk8UDYA5YaFpco6Z5xVF'
